0da3242822db6fcdc4d30f91c3094d22803f1259
Niko Virtala <niko.virtala@hey.com>
2024-11-24 14:15:42 +0200

    chore: file/folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
-import { App, Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-
-export class MyStack extends Stack {
-    constructor(scope: Construct, id: string, props: StackProps = {}) {
-        super(scope, id, props);
-
-        // define resources here...
-    }
-}
-
-// for development, use account/region from cdk cli
-const devEnv = {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-};
+import { App } from "aws-cdk-lib";
+import { MyStack } from "@/stacks";
 
 const app = new App();
 
-new MyStack(app, "cloudformation-hooks-custom-lambda-dev", { env: devEnv });
-// new MyStack(app, 'cloudformation-hooks-custom-lambda-prod', { env: prodEnv });
+new MyStack(app, "cloudformation-hooks-custom-lambda");
 
 app.synth();
